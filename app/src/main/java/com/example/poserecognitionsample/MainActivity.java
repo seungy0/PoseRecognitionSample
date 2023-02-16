d7d6542b716d8699839c72b914e37910da254110
seunggyuV <seunggyu@vaultmicro.com>
2023-02-16 11:21:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/poserecognitionsample/MainActivity.java
@@ -0,0 +1,259 @@
+package com.example.poserecognitionsample;
+
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.mediapipe.components.CameraHelper;
+import com.google.mediapipe.components.CameraXPreviewHelper;
+import com.google.mediapipe.components.ExternalTextureConverter;
+import com.google.mediapipe.components.FrameProcessor;
+import com.google.mediapipe.components.PermissionHelper;
+import com.google.mediapipe.formats.proto.LandmarkProto;
+import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.PacketGetter;
+import com.google.mediapipe.glutil.EglManager;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+/** Main activity of MediaPipe basic app. */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String BINARY_GRAPH_NAME = "pose_tracking_gpu.binarypb";
+    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
+    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
+    private static final String OUTPUT_LANDMARKS_STREAM_NAME = "pose_landmarks";
+    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
+    // Flips the camera-preview frames vertically by default, before sending them into FrameProcessor
+    // to be processed in a MediaPipe graph, and flips the processed frames back when they are
+    // displayed. This maybe needed because OpenGL represents images assuming the image origin is at
+    // the bottom-left corner, whereas MediaPipe in general assumes the image origin is at the
+    // top-left corner.
+    // NOTE: use "flipFramesVertically" in manifest metadata to override this behavior.
+    private static final boolean FLIP_FRAMES_VERTICALLY = true;
+
+    // Number of output frames allocated in ExternalTextureConverter.
+    // NOTE: use "converterNumBuffers" in manifest metadata to override number of buffers. For
+    // example, when there is a FlowLimiterCalculator in the graph, number of buffers should be at
+    // least `max_in_flight + max_in_queue + 1` (where max_in_flight and max_in_queue are used in
+    // FlowLimiterCalculator options). That's because we need buffers for all the frames that are in
+    // flight/queue plus one for the next frame from the camera.
+    private static final int NUM_BUFFERS = 2;
+
+    static {
+        // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni");
+        try {
+            System.loadLibrary("opencv_java3");
+        } catch (java.lang.UnsatisfiedLinkError e) {
+            // Some example apps (e.g. template matching) require OpenCV 4.
+            System.loadLibrary("opencv_java4");
+        }
+    }
+
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    protected FrameProcessor processor;
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    protected CameraXPreviewHelper cameraHelper;
+
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private SurfaceTexture previewFrameTexture;
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private SurfaceView previewDisplayView;
+
+    // Creates and manages an {@link EGLContext}.
+    private EglManager eglManager;
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private ExternalTextureConverter converter;
+
+    // ApplicationInfo for retrieving metadata defined in the manifest.
+    private ApplicationInfo applicationInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewLayoutResId());
+
+        try {
+            applicationInfo =
+                    getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            Log.e(TAG, "Cannot find application info: " + e);
+        }
+
+        previewDisplayView = new SurfaceView(this);
+        setupPreviewDisplayView();
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this);
+        eglManager = new EglManager(null);
+        processor =
+                new FrameProcessor(
+                        this,
+                        eglManager.getNativeContext(),
+                        BINARY_GRAPH_NAME,
+                        INPUT_VIDEO_STREAM_NAME,
+                        OUTPUT_VIDEO_STREAM_NAME);
+        processor
+                .getVideoSurfaceOutput()
+                .setFlipY(FLIP_FRAMES_VERTICALLY);
+
+        PermissionHelper.checkAndRequestCameraPermissions(this);
+
+        if (Log.isLoggable(TAG, Log.VERBOSE)) {
+            processor.addPacketCallback(
+                    OUTPUT_LANDMARKS_STREAM_NAME,
+                    (packet) -> {
+                        Log.v(TAG, "Received pose landmarks packet.");
+                        try {
+                            LandmarkProto.NormalizedLandmarkList poseLandmarks =
+                                    PacketGetter.getProto(packet, LandmarkProto.NormalizedLandmarkList.class);
+                            Log.v(
+                                    TAG,
+                                    "[TS:"
+                                            + packet.getTimestamp()
+                                            + "] "
+                                            + getPoseLandmarksDebugString(poseLandmarks));
+                        } catch (InvalidProtocolBufferException exception) {
+                            Log.e(TAG, "Failed to get proto.", exception);
+                        }
+                    });
+        }
+    }
+
+    private static String getPoseLandmarksDebugString(LandmarkProto.NormalizedLandmarkList poseLandmarks) {
+        String poseLandmarkStr = "Pose landmarks: " + poseLandmarks.getLandmarkCount() + "\n";
+        int landmarkIndex = 0;
+        for (LandmarkProto.NormalizedLandmark landmark : poseLandmarks.getLandmarkList()) {
+            poseLandmarkStr +=
+                    "\tLandmark ["
+                            + landmarkIndex
+                            + "]: ("
+                            + landmark.getX()
+                            + ", "
+                            + landmark.getY()
+                            + ", "
+                            + landmark.getZ()
+                            + ")\n";
+            ++landmarkIndex;
+        }
+        return poseLandmarkStr;
+    }
+
+    // Used to obtain the content view for this application. If you are extending this class, and
+    // have a custom layout, override this method and return the custom layout.
+    protected int getContentViewLayoutResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        converter =
+                new ExternalTextureConverter(
+                        eglManager.getContext(), 2);
+        converter.setFlipY(FLIP_FRAMES_VERTICALLY);
+        converter.setConsumer(processor);
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        converter.close();
+
+        // Hide preview display until we re-open the camera again.
+        previewDisplayView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    protected void onCameraStarted(SurfaceTexture surfaceTexture) {
+        previewFrameTexture = surfaceTexture;
+        // Make the display view visible to start showing the preview. This triggers the
+        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+        previewDisplayView.setVisibility(View.VISIBLE);
+    }
+
+    protected Size cameraTargetResolution() {
+        return null; // No preference and let the camera (helper) decide.
+    }
+
+    public void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        previewFrameTexture = converter.getSurfaceTexture();
+        cameraHelper.setOnCameraStartedListener(
+                surfaceTexture -> {
+                    onCameraStarted(surfaceTexture);
+                });
+        CameraHelper.CameraFacing cameraFacing = CameraHelper.CameraFacing.FRONT;
+        cameraHelper.startCamera(
+                this, cameraFacing, previewFrameTexture, cameraTargetResolution());
+    }
+
+    protected Size computeViewSize(int width, int height) {
+        return new Size(width, height);
+    }
+
+    protected void onPreviewDisplaySurfaceChanged(
+            SurfaceHolder holder, int format, int width, int height) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        Size viewSize = computeViewSize(width, height);
+        Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+        boolean isCameraRotated = cameraHelper.isCameraRotated();
+
+        // Configure the output width and height as the computed display size.
+        converter.setDestinationSize(
+                isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
+                isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+    }
+
+    private void setupPreviewDisplayView() {
+        previewDisplayView.setVisibility(View.GONE);
+        ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
+        viewGroup.addView(previewDisplayView);
+
+        previewDisplayView
+                .getHolder()
+                .addCallback(
+                        new SurfaceHolder.Callback() {
+                            @Override
+                            public void surfaceCreated(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+                            }
+
+                            @Override
+                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                                onPreviewDisplaySurfaceChanged(holder, format, width, height);
+                            }
+
+                            @Override
+                            public void surfaceDestroyed(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(null);
+                            }
+                        });
+    }
+}
